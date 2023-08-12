dd6570a9cf5aa43687bde92f015e9a024ceb0d88
nabeel244 <nabeel.y@mytechnology-team.com>
2023-08-13 01:57:31 +0500

    complete project without compress any file

---
+++ asset/main.js
@@ -0,0 +1,243 @@
+/**
+ * Template Name: KnightOne - v2.2.0
+ * Template URL: https://bootstrapmade.com/knight-simple-one-page-bootstrap-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+!(function ($) {
+  "use strict";
+
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  var scrolltoOffset = $("#header").outerHeight() - 2;
+  $(document).on(
+    "click",
+    ".nav-menu a, .mobile-nav a, .scrollto",
+    function (e) {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        if (target.length) {
+          e.preventDefault();
+
+          var scrollto = target.offset().top - scrolltoOffset;
+
+          if ($(this).attr("href") == "#header") {
+            scrollto = 0;
+          }
+
+          $("html, body").animate(
+            {
+              scrollTop: scrollto,
+            },
+            1500,
+            "easeInOutExpo"
+          );
+
+          if ($(this).parents(".nav-menu, .mobile-nav").length) {
+            $(".nav-menu .active, .mobile-nav .active").removeClass("active");
+            $(this).closest("li").addClass("active");
+          }
+
+          if ($("body").hasClass("mobile-nav-active")) {
+            $("body").removeClass("mobile-nav-active");
+            $(".mobile-nav-toggle i").toggleClass(
+              "icofont-navigation-menu icofont-close"
+            );
+            $(".mobile-nav-overly").fadeOut();
+          }
+          return false;
+        }
+      }
+    }
+  );
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function () {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $("html, body").animate(
+          {
+            scrollTop: scrollto,
+          },
+          1500,
+          "easeInOutExpo"
+        );
+      }
+    }
+  });
+
+  // Mobile Navigation
+  if ($(".nav-menu").length) {
+    var $mobile_nav = $(".nav-menu").clone().prop({
+      class: "mobile-nav d-lg-none",
+    });
+    $("body").append($mobile_nav);
+    $("body").prepend(
+      '<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>'
+    );
+    $("body").append('<div class="mobile-nav-overly"></div>');
+
+    $(document).on("click", ".mobile-nav-toggle", function (e) {
+      $("body").toggleClass("mobile-nav-active");
+      $(".mobile-nav-toggle i").toggleClass(
+        "icofont-navigation-menu icofont-close"
+      );
+      $(".mobile-nav-overly").toggle();
+    });
+
+    $(document).on("click", ".mobile-nav .drop-down > a", function (e) {
+      e.preventDefault();
+      $(this).next().slideToggle(300);
+      $(this).parent().toggleClass("active");
+    });
+
+    $(document).click(function (e) {
+      var container = $(".mobile-nav, .mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $(".mobile-nav-toggle i").toggleClass(
+            "icofont-navigation-menu icofont-close"
+          );
+          $(".mobile-nav-overly").fadeOut();
+        }
+      }
+    });
+  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+    $(".mobile-nav, .mobile-nav-toggle").hide();
+  }
+
+  // Navigation active state on scroll
+  var nav_sections = $("section");
+  var main_nav = $(".nav-menu, #mobile-nav");
+
+  $(window).on("scroll", function () {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function () {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find("li").removeClass("active");
+        }
+        main_nav
+          .find('a[href="#' + $(this).attr("id") + '"]')
+          .parent("li")
+          .addClass("active");
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass("active");
+      }
+    });
+  });
+
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#header").addClass("header-scrolled");
+    } else {
+      $("#header").removeClass("header-scrolled");
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $("#header").addClass("header-scrolled");
+  }
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      "easeInOutExpo"
+    );
+    return false;
+  });
+
+  $(document).ready(function () {
+    var $chatbox = $(".chatbox"),
+      $chatboxTitle = $(".chatbox__title"),
+      $chatboxTitleClose = $(".chatbox__title__close"),
+      $chatboxCredentials = $(".chatbox__credentials");
+    $chatboxTitle.on("click", function () {
+      $chatbox.toggleClass("chatbox--tray");
+    });
+    $chatboxTitleClose.on("click", function (e) {
+      e.stopPropagation();
+      $chatbox.addClass("chatbox--closed");
+    });
+    $chatbox.on("transitionend", function () {
+      if ($chatbox.hasClass("chatbox--closed")) $chatbox.remove();
+    });
+    $chatboxCredentials.on("submit", function (e) {
+      e.preventDefault();
+      $chatbox.removeClass("chatbox--empty");
+    });
+  });
+  $("#socialbtn").click(function () {
+    $("#modalchat").toggle();
+  });
+  $(function () {
+    var $a = $(".tabs li");
+    $a.click(function () {
+      $a.removeClass("active");
+      $(this).addClass("active");
+    });
+  });
+  $(function () {
+    var $a = $(".tabs1 li");
+    $a.click(function () {
+      $a.removeClass("active");
+      $(this).addClass("active");
+    });
+  });
+})(jQuery);
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion) => {
+  const question = accordion.querySelector(".accordion__question");
+  const answer = accordion.querySelector(".accordion__answer");
+  const icon = question.querySelector("i");
+
+  question.addEventListener("click", () => {
+    accordion.classList.toggle("active");
+    if (accordion.classList.contains("active")) {
+      answer.style.display = "block";
+      icon.classList.remove("fa-caret-down");
+      icon.classList.add("fa-caret-up");
+    } else {
+      answer.style.display = "none";
+      icon.classList.remove("fa-caret-up");
+      icon.classList.add("fa-caret-down");
+    }
+  });
+});
---
+++ asset/model.js
@@ -0,0 +1,168 @@
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+var modal1 = document.getElementById("myModal1");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg1");
+var modalImg1 = document.getElementById("img02");
+var captionText1 = document.getElementById("caption");
+
+img.onclick = function() {
+    modal1.style.display = "block";
+    modalImg1.src = this.src;
+    modalImg1.alt = this.alt;
+    captionText1.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+var modal2 = document.getElementById("myModal2");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("img03");
+var captionText2 = document.getElementById("caption");
+
+img.onclick = function() {
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+    modalImg2.alt = this.alt;
+    captionText2.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+var modal3 = document.getElementById("myModal3");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg3");
+var modalImg3 = document.getElementById("img04");
+var captionText3 = document.getElementById("caption");
+
+img.onclick = function() {
+    modal3.style.display = "block";
+    modalImg3.src = this.src;
+    modalImg2.alt = this.alt;
+    captionText3.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+var modal4 = document.getElementById("myModal4");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg4");
+var modalImg4 = document.getElementById("img05");
+var captionText4 = document.getElementById("caption");
+
+img.onclick = function() {
+    modal4.style.display = "block";
+    modalImg4.src = this.src;
+    modalImg2.alt = this.alt;
+    captionText4.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+var modal5 = document.getElementById("myModal5");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg5");
+var modalImg5 = document.getElementById("img06");
+var captionText5 = document.getElementById("caption");
+
+img.onclick = function() {
+    modal5.style.display = "block";
+    modalImg5.src = this.src;
+    modalImg2.alt = this.alt;
+    captionText5.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+var modal6 = document.getElementById("myModal6");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg6");
+var modalImg6 = document.getElementById("img07");
+var captionText6 = document.getElementById("caption");
+
+img.onclick = function() {
+    modal6.style.display = "block";
+    modalImg6.src = this.src;
+    modalImg2.alt = this.alt;
+    captionText6.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+var modal7 = document.getElementById("myModal7");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg7");
+var modalImg7 = document.getElementById("img08");
+var captionText7 = document.getElementById("caption");
+
+img.onclick = function() {
+    modal7.style.display = "block";
+    modalImg7.src = this.src;
+    modalImg2.alt = this.alt;
+    captionText7.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
